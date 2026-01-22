a17562f42618d8f71dc66aad3419f16adccae050
Andrew Plotkin <zarf@ZarfLent.local>
2026-01-21 19:49:26 -0500

    Let's do this the modern way.

---
+++ beaconlib/__main__.py
@@ -0,0 +1,43 @@
+import sys
+import argparse
+
+srclines = None
+
+def parse(filename):
+    global srclines
+    
+    fl = open(filename)
+    parsetrees, srclines = parselines(fl)
+    fl.close()
+
+    if args.showterms:
+        for term in parsetrees:
+            term.dump()
+        
+    return compileall(parsetrees)
+
+
+if __name__ == '__main__':
+    from beaconlib.lex import parselines
+    from beaconlib.compile import compileall
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('filename')
+    parser.add_argument('--showterms', action='store_true')
+    parser.add_argument('--shownodes', action='store_true')
+    
+    args = parser.parse_args()
+
+
+    program = parse(args.filename)
+    program.post()
+    if args.shownodes:
+        program.dump()
+    
+    print('// ' + args.filename)
+    if False:
+        for ln in srclines:
+            print('// ' + ln)
+        print()
+    program.write()
