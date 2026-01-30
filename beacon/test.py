386d09307ef4e218c7b5279e44a9850540730df3
Andrew Plotkin <zarf@ZarfLent.local>
2026-01-29 21:15:22 -0500

    Converted all tests.

--- beacon/test.py
+++ beacon/test.py
@@ -66,7 +66,6 @@ class TestCompile(unittest.TestCase):
         output = stripdown(outfl.getvalue())
 
         self.assertEqual(output, res)
-        
 
     def compile(self, src):
         fl = StringIO(src)
@@ -76,16 +75,6 @@ class TestCompile(unittest.TestCase):
         program = compileall(parsetrees, srclines=srclines)
         program.post()
         return program
-
-    def compare(self, src, template):
-        prog = self.compile(src)
-        outfl = StringIO()
-        prog.write(outfl)
-        res = outfl.getvalue()
-        
-        res = stripdown(res)
-        template = template.strip()
-        self.assertEqual(res, template)
 
     def test_constant(self):
         self.checkfile('constant.pbb')
@@ -114,251 +103,15 @@ class TestCompile(unittest.TestCase):
     def test_sumscalarcolor2(self):
         self.checkfile('sumscalarcolor2.pbb')
         
-    def test_pulser_decayinplace(self):
-        src = deindent('''
-        pulser:
-          maxcount=4
-          spaceshape=triangle
-          width=0.3
-          timeshape=sawdecay
-          duration=0.2
-        ''')
-
-        self.compare(src, '''
-var pulser_0_live = array(4)
-var pulser_0_birth = array(4)
-var pulser_0_livecount = 0
-var pulser_0_nextstart = 0
-var pulser_0_vector = array(pixelCount)
-export function beforeRender(delta) {
-  clock += (delta / 1000)
-  for (var ix=0; ix<pixelCount; ix++) {
-    pulser_0_vector[ix] = (0)
-  }
-  if (clock >= pulser_0_nextstart && pulser_0_livecount < 4) {
-    for (var px=0; px<4; px++) {
-      if (!pulser_0_live[px]) { break }
-    }
-    if (px < 4) {
-      pulser_0_live[px] = 1
-      livecount += 1
-      pulser_0_nextstart = clock + 1
-      pulser_0_birth[px] = clock
-    }
-  }
-  for (var px=0; px<4; px++) {
-    if (!pulser_0_live[px]) { break }
-    age = clock - pulser_0_birth[px]
-    relage = age / 0.2
-    if (relage > 1.0) {
-      pulser_0_live[px] = 0
-      livecount -= 1
-      continue
-    }
-    timeval = (1-relage)
-    ppos = 0.5
-    pwidth = 0.3
-    minpos = max(0, pixelCount*(ppos-pwidth/2))
-    maxpos = min(pixelCount, pixelCount*(ppos+pwidth/2))
-    for (var ix=minpos; ix<maxpos; ix++) {
-      relpos = ((ix/pixelCount)-(ppos-pwidth/2)) / pwidth
-      spaceval = triangle(relpos)
-      pulser_0_vector[ix] += (timeval * spaceval)
-    }
-  }
-}
-export function render(index) {
-  var val = pulser_0_vector[index]
-  rgb(val*val, val*val, val*val)
-}
-        ''')
-
     def test_pulser_randpos(self):
-        src = deindent('''
-        pulser:
-          maxcount=4
-          spaceshape=triangle
-          width=0.3
-          timeshape=sawdecay
-          duration=0.2
-          pos=randflat: 0.2, 0.8
-        ''')
-
-        self.compare(src, '''
-var pulser_0_live = array(4)
-var pulser_0_birth = array(4)
-var pulser_0_livecount = 0
-var pulser_0_nextstart = 0
-var pulser_0_arg_pos = array(4)
-var pulser_0_vector = array(pixelCount)
-export function beforeRender(delta) {
-  clock += (delta / 1000)
-  for (var ix=0; ix<pixelCount; ix++) {
-    pulser_0_vector[ix] = (0)
-  }
-  if (clock >= pulser_0_nextstart && pulser_0_livecount < 4) {
-    for (var px=0; px<4; px++) {
-      if (!pulser_0_live[px]) { break }
-    }
-    if (px < 4) {
-      pulser_0_live[px] = 1
-      livecount += 1
-      randflat_3_val_min = 0.2
-      randflat_3_val_diff = (0.8-randflat_3_val_min)
-      pulser_0_arg_pos[px] = (random(randflat_3_val_diff)+randflat_3_val_min)
-      pulser_0_nextstart = clock + 1
-      pulser_0_birth[px] = clock
-    }
-  }
-  for (var px=0; px<4; px++) {
-    if (!pulser_0_live[px]) { break }
-    age = clock - pulser_0_birth[px]
-    relage = age / 0.2
-    if (relage > 1.0) {
-      pulser_0_live[px] = 0
-      livecount -= 1
-      continue
-    }
-    timeval = (1-relage)
-    ppos = pulser_0_arg_pos[px]
-    pwidth = 0.3
-    minpos = max(0, pixelCount*(ppos-pwidth/2))
-    maxpos = min(pixelCount, pixelCount*(ppos+pwidth/2))
-    for (var ix=minpos; ix<maxpos; ix++) {
-      relpos = ((ix/pixelCount)-(ppos-pwidth/2)) / pwidth
-      spaceval = triangle(relpos)
-      pulser_0_vector[ix] += (timeval * spaceval)
-    }
-  }
-}
-export function render(index) {
-  var val = pulser_0_vector[index]
-  rgb(val*val, val*val, val*val)
-}
-        ''')
-
+        self.checkfile('pulser_randpos.pbb')
+        
     def test_pulser_quoterandpos(self):
-        src = deindent('''
-        pulser:
-          maxcount=4
-          spaceshape=triangle
-          width=0.3
-          timeshape=sawdecay
-          duration=0.2
-          pos=quote:randflat: 0.2, 0.8
-        ''')
-
-        self.compare(src, '''
-var pulser_0_live = array(4)
-var pulser_0_birth = array(4)
-var pulser_0_livecount = 0
-var pulser_0_nextstart = 0
-var pulser_0_vector = array(pixelCount)
-export function beforeRender(delta) {
-  clock += (delta / 1000)
-  for (var ix=0; ix<pixelCount; ix++) {
-    pulser_0_vector[ix] = (0)
-  }
-  if (clock >= pulser_0_nextstart && pulser_0_livecount < 4) {
-    for (var px=0; px<4; px++) {
-      if (!pulser_0_live[px]) { break }
-    }
-    if (px < 4) {
-      pulser_0_live[px] = 1
-      livecount += 1
-      pulser_0_nextstart = clock + 1
-      pulser_0_birth[px] = clock
-    }
-  }
-  for (var px=0; px<4; px++) {
-    if (!pulser_0_live[px]) { break }
-    age = clock - pulser_0_birth[px]
-    relage = age / 0.2
-    if (relage > 1.0) {
-      pulser_0_live[px] = 0
-      livecount -= 1
-      continue
-    }
-    timeval = (1-relage)
-    randflat_4_val_min = 0.2
-    randflat_4_val_diff = (0.8-randflat_4_val_min)
-    ppos = (random(randflat_4_val_diff)+randflat_4_val_min)
-    pwidth = 0.3
-    minpos = max(0, pixelCount*(ppos-pwidth/2))
-    maxpos = min(pixelCount, pixelCount*(ppos+pwidth/2))
-    for (var ix=minpos; ix<maxpos; ix++) {
-      relpos = ((ix/pixelCount)-(ppos-pwidth/2)) / pwidth
-      spaceval = triangle(relpos)
-      pulser_0_vector[ix] += (timeval * spaceval)
-    }
-  }
-}
-export function render(index) {
-  var val = pulser_0_vector[index]
-  rgb(val*val, val*val, val*val)
-}
-        ''')
-
-    def test_pulser_gradient(self):
-        src = deindent('''
-        gradient:
-          stop: 0, $000
-          stop: 0.25, $060
-          stop: 1.0, $FCF
-          mul:
-            wave: sine
-            time: wave: sine
-        ''')
-
-        self.compare(src, '''
-function evalGradient(val, posls, colls, count)
-{
-  if (val <= posls[0]) {
-    return colls[0]
-  }
-  if (val >= posls[count-1]) {
-    return colls[count-1]
-  }
-  for (var ix=0; ix<count-1; ix++) {
-    if (val < posls[ix+1]) {
-      return mix(colls[ix], colls[ix+1], (val-posls[ix])/(posls[ix+1]-posls[ix]))
-    }
-  }
-  return colls[count-1]
-}
-var gradient_0_grad_pos = [0.0, 0.25, 1.0]
-var gradient_0_grad_r = [0.0, 0.0, 1.0]
-var gradient_0_grad_g = [0.0, 0.4, 0.8]
-var gradient_0_grad_b = [0.0, 0.0, 1.0]
-var time_9_scalar
-var wave_5_vector = array(pixelCount)
-var gradient_0_vector_r = array(pixelCount)
-var gradient_0_vector_g = array(pixelCount)
-var gradient_0_vector_b = array(pixelCount)
-for (var ix=0; ix<pixelCount; ix++) {
-  var wave_5_val_min = 0  // for wave_5
-  var wave_5_val_hdiff = ((1-wave_5_val_min)*0.5)  // for wave_5
-  wave_5_vector[ix] = ((wave_5_val_min+wave_5_val_hdiff*(1-cos(PI2*(((ix/pixelCount)-0.5)/1+0.5)))))
-}
-export function beforeRender(delta) {
-  clock += (delta / 1000)
-  var wave_10_val_min = 0  // for time_9
-  var wave_10_val_hdiff = ((1-wave_10_val_min)*0.5)  // for time_9
-  time_9_scalar = ((wave_10_val_min+wave_10_val_hdiff*(1-cos(PI2*clock/1))))
-  for (var ix=0; ix<pixelCount; ix++) {
-    gradient_0_vector_r[ix] = (evalGradient((wave_5_vector[ix] * time_9_scalar), gradient_0_grad_pos, gradient_0_grad_r, 3))
-    gradient_0_vector_g[ix] = (evalGradient((wave_5_vector[ix] * time_9_scalar), gradient_0_grad_pos, gradient_0_grad_g, 3))
-    gradient_0_vector_b[ix] = (evalGradient((wave_5_vector[ix] * time_9_scalar), gradient_0_grad_pos, gradient_0_grad_b, 3))
-  }
-}
-export function render(index) {
-  var valr = gradient_0_vector_r[index]
-  var valg = gradient_0_vector_g[index]
-  var valb = gradient_0_vector_b[index]
-  rgb(valr*valr, valg*valg, valb*valb)
-}
-        ''')
-
+        self.checkfile('pulser_quoterandpos.pbb')
+        
+    def test_gradient(self):
+        self.checkfile('gradient.pbb')
+        
 
 if __name__ == '__main__':
     unittest.main()
